104282341f34da06fc81230118b82e2d85c253ac
Blue-bird <swchogh@gmail.com>
2024-10-29 02:53:25 +0900

    [D1] Title: 중간값 찾기, Time: 160 ms, Memory: 21,132 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,24 +1,22 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
-class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T = sc.nextInt();
-		sc.nextLine();
-
-		if (T % 2 != 1 || T < 9 || T > 199) {
-			System.out.println("N은 9 이상 199 이하의 홀수만 입력 가능합니다.");
-		}
-
-		String[] input = sc.nextLine().split(" ");
-		int[] numbers = java.util.Arrays.stream(input)
-				.mapToInt(Integer::parseInt)
-				.toArray();
-		Arrays.sort(numbers);
-
-		sc.close();
-		System.out.println(numbers[T/2]);
-	}
+ 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int N = sc.nextInt();
+ 
+        int[] arrayNum = new int[N];
+ 
+        for (int i = 0; i < N; i++) {
+            arrayNum[i] = sc.nextInt();
+        }
+ 
+        Arrays.sort(arrayNum);
+ 
+        int middleNum = arrayNum[N / 2];
+        System.out.println(middleNum);
+ 
+    }
 }
