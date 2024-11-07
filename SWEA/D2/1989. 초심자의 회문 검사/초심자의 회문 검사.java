9d5aa8ba09a0314779bd6a047e0b6d7950816b9a
Blue-bird <swchogh@gmail.com>
2024-11-07 23:52:44 +0900

    [D2] Title: 초심자의 회문 검사, Time: 140 ms, Memory: 20,532 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		sc.nextLine();
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        sc.nextLine();
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			String input = sc.nextLine();
-			char[] palindrome = input.toCharArray();
-			int check = 0;
-			
-			for (int i = 0; i < palindrome.length / 2; i++) {
-				if (palindrome[i] == palindrome[palindrome.length - i - 1]) {
-					check = 1;
-				}
-			}
-			
-			System.out.println("#" + test_case + " " + check);
-		}
+        for (int test_case = 1; test_case <= T; test_case++) {
+            String input = sc.nextLine();
+            boolean isPalindrome = checkPalindrome(input);
+            
+            System.out.println("#" + test_case + " " + (isPalindrome ? 1 : 0));
+        }
+    }
+
+    private static boolean checkPalindrome(String input) {
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+    	}
+        return true;
 	}
 }
