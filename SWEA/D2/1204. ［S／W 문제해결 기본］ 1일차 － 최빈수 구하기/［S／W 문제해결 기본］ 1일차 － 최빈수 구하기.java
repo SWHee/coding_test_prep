8d997101da9ad6de05308d50c78cd5fec046a948
Blue-bird <swchogh@gmail.com>
2024-11-17 01:42:42 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 231 ms, Memory: 40,584 KB -BaekjoonHub

---
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int testCase = sc.nextInt();
+            int[] check = new int[101]; // 점수는 0부터 100까지이므로, 배열 크기는 101로 선언
+            int mostMany = 0;  // 최빈수의 빈도
+            int mostManyScore = 0;  // 최빈수의 점수
+            
+            // 점수 입력받기 및 빈도 계산
+            for (int i = 0; i < 1000; i++) {
+                int score = sc.nextInt();
+                if (score < 0 || score > 100) {
+                    System.out.println("점수는 0점 이상 100점 이하의 숫자만 입력 가능합니다.");
+                } else {
+                    check[score]++; // 점수에 해당하는 빈도 증가
+                }
+            }
+            
+            // 최빈수 계산: 빈도가 가장 높은 점수 찾기
+            for (int i = 0; i <= 100; i++) {
+                if (check[i] > mostMany) {
+                    mostMany = check[i];
+                    mostManyScore = i;
+                } else if (check[i] == mostMany && i > mostManyScore) {
+                    mostManyScore = i; // 빈도가 같으면 더 큰 점수를 선택
+                }
+            }
+            
+            // 결과 출력
+            System.out.println("#" + testCase + " " + mostManyScore);
+        }
+        
+        sc.close();
+    }
+}
