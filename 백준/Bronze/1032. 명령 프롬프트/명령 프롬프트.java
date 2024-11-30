506f63fcfc26e15d594a3f3f94ea6b350f94b467
Blue-bird <swchogh@gmail.com>
2024-11-30 17:15:23 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -5,28 +5,22 @@ class Main {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		String files[] = new String[N];
+		// 기준이 되는 첫번째 기준
+		char[] chArray = br.readLine().toCharArray();
+		
+		// 입력받는 하나의 문자열마다 비교, chArray가 첫번째이므로 i=1부터 시작
+		for (int i = 1; i < N; i++) {
+			String str = br.readLine();
+			int idx = 0; 
 
-		// N개의 파일명 입력받기
-		for (int i = 0; i < N; i++) {
-			files[i] = br.readLine();
-		}
-
-		int nameLength = files[0].length();
-		if (nameLength > 50)
-			return;
-
-		char[] pattern = files[0].toCharArray();
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < nameLength; j++) {
-				if (pattern[j] != files[i].charAt(j)) {
-					pattern[j] = '?'; // 다르면 '?'로 대체
+			while (idx < chArray.length) {
+				if (chArray[idx] != str.charAt(idx)) {
+					chArray[idx] = '?';
 				}
+				idx++;
 			}
 		}
 
-		System.out.println(new String(pattern));
-		br.close();
+		System.out.println(new String(chArray));
 	}
 }
