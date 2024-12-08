a849d9619af66cd928e6a594ce53f56aa93176f7
Blue-bird <swchogh@gmail.com>
2024-12-08 15:29:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 460 ms, Memory: 106296 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] numbers = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] input = br.readLine().split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(input[i]);
+            if(num < min) min = num;
+            if(num > max) max = num;
         }
 
-        Arrays.sort(numbers);
-
-        System.out.println(numbers[0] + " " + numbers[N - 1]);
+        System.out.println(min + " " + max);
     }
 }
