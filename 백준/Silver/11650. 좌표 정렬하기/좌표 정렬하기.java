cd608b99f65bd7cafc45c8dc3e22b1400cfa5933
Blue-bird <swchogh@gmail.com>
2024-12-12 13:36:55 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1216 ms, Memory: 65860 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -22,29 +22,27 @@ public class Main {
 
         }
 
-//        Arrays.sort(points, new Comparator<int[]>() {
-//            @Override
-//            public int compare(int[] o1, int[] o2) {
-//                if (o1[0] == o2[0]){
-//                    return o1[1] - o2[1];
-//                } else {
-//                    return o1[0] - o2[0];
-//                }
-//            }
-//        });
-        
-        Arrays.sort(points, (o1, o2) -> {
-            if(o1[0] == o2[0]){
-                return o1[1] - o2[1];
-            } else {
-                return o1[0] - o2[0];
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
             }
         });
+
+//        Arrays.sort(points, (o1, o2) -> {
+//            if(o1[0] == o2[0]){
+//                return o1[1] - o2[1];
+//            } else {
+//                return o1[0] - o2[0];
+//            }
+//        });
 
         for(int[] point : points) {
             System.out.println(point[0] + " " + point[1]);
         }
-
-
     }
 }
