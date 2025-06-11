1a40afdd5b003721a4fedd0e0ffea3f02a7ed848
Blue-bird <swchogh@gmail.com>
2025-06-11 17:25:10 +0900

    [level 2] Title: 올바른 괄호, Time: 20.66 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,32 +1,23 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         // 조건을 충족하기 위한 조건
         // 연속된 "("의 개수만큼 ")"이 나온 이후에 다른 "("가 나올 수 있다
         // 연속된 "("의 개수만큼 ")"이 나오지 않는다면 false 처리
+        Stack<Character> st = new Stack<>();
         boolean answer = true;
         
-        String[] words = s.split("");
-        
-        int seqLeft = 0;
-        int seqRight = 0;
-        
-        for(int i = 0; i < words.length; i++) {
-            // ')'이 바로 나올경우 무조건 false 이므로 시간 절약
-            if(words[0].equals(")")) return false;
-            
-            if(words[i].equals("(")) {
-                seqLeft++;
-            } else if(words[i].equals(")")) {
-                seqRight++;
-            }
-            
-            if(seqRight > seqLeft) {
-                answer = false;
-                break;
+        char[] words = s.toCharArray();
+        for(int i = 0; i < s.length(); i++) {
+            if(words[i] == '('){
+                st.push(words[i]);
+            } else if(words[i] == ')') {
+                if(st.isEmpty()) return false;
+                st.pop();
             }
         }
-        if(seqLeft != seqRight) answer = false;
-
-        return answer;
+                      
+        return st.isEmpty();
     }
 }
