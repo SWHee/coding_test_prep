558d09af394b55c5925ce9b0032868deab24568e
Blue-bird <swchogh@gmail.com>
2025-09-24 18:42:07 +0900

    [Silver IV] Title: 스택, Time: 172 ms, Memory: 20876 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            String cmd = br.readLine();
+            if (cmd.startsWith("push")) {
+                int x = Integer.parseInt(cmd.split(" ")[1]);
+                stack.push(x);
+            } else if (cmd.equals("pop")) {
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
+            } else if (cmd.equals("size")) {
+                sb.append(stack.size()).append('\n');
+            } else if (cmd.equals("empty")) {
+                sb.append(stack.isEmpty() ? 1 : 0).append('\n');
+            } else if (cmd.equals("top")) {
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
