09e2175b19bc0d251ab272181e6053d352e49112
Blue-bird <swchogh@gmail.com>
2025-11-22 02:28:36 +0900

    [D2] Title: 간단한 369게임, Time: 88 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,32 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    // 입력 값
 
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {   // 입력 값 1 ~ N 까지
+            // 33이 들어왔다고 생각해보자
+            String strNum = Integer.toString(i);
+            int clapCount = 0;  // 박수 쳐야하는 횟수
 
-		for (int i = 1; i <= N; i++) {
-			String number = Integer.toString(i);
-			int clapCount = 0;
+            for (int j = 0; j < strNum.length(); j++) {   // 몇번 - 출력해야하는지 구하는 부분 -> clapCount
+                char c = strNum.charAt(j);
 
-			for (int j = 0; j < number.length(); j++) {
-				char c = number.charAt(j);
-				if (c == '3' || c == '6' || c == '9') {
-					clapCount++;
-				}
-			}
+                if (c == '3' || c == '6' || c == '9') {  // 해당 자리 숫자가 3, 6, 9 중 하나이면
+                    clapCount++;
+                }
+            }
 
-			if (clapCount > 0) {
-				for (int k = 0; k < clapCount; k++) {
-					System.out.print("-");
-				}
-			} else
-				System.out.print(i);
-			
-			System.out.print(" ");
+            if (clapCount > 0) {
+                // '-'를 clapCount만큼 출력
+                for (int k = 0; k < clapCount; k++) {
+                    System.out.print("-");
+                }
+            } else {
+                System.out.print(i);
+            }
 
-		}
-		sc.close();
-	}
+            System.out.print(" ");
+        }
+    }
 }
